ae52290d6dcc98cf7d72919aeaba98e5462be29a
qoeroas <135075620+qoeroas@users.noreply.github.com>
2023-05-30 20:04:29 -0300

    Update d.d

--- dd/d/dd/d/dd/d/dd/d/dd/dddddd/d/ddd/d/dd/dddd/ddddddd/dd/d/ddd/d/dd/d/dd/d/dd/ddd/d.d
+++ dd/d/dd/d/dd/d/dd/d/dd/dddddd/d/ddd/d/dd/dddd/ddddddd/dd/d/ddd/d/dd/d/dd/d/dd/ddd/d.d
@@ -1,8 +1,9 @@
+// sorting : // Y // M // D //
 World Name: DYNAMO Owner's Last Logged In Date: 2023-04-05 9:57:47 PM
 World Name: MASTER Owner's Last Logged In Date: 2023-01-05 1:54:18 PM
 World Name: CLUE Owner's Last Logged In Date: 2023-02-13 9:30:43 PM
 World Name: GUARD Owner's Last Logged In Date: 2022-11-18 3:22:03 PM
-World Name: TURF Owner's Last Logged In Date: 8/23/2022 8:09:02 PM
+World Name: TURF Owner's Last Logged In Date: 2022-8-23 8:09:02 PM
 World Name: TIGER Owner's Last Logged In Date: 2022-11-13 7:13:56 AM
 World Name: REDHERRING Owner's Last Logged In Date: 2022-09-17 2:47:16 PM
 World Name: BUYPET Owner's Last Logged In Date: 2022-12-08 3:58:36 PM
